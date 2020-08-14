af897a7164db53064d8c3aea0516139de0471b04
LevikV <alex@kplus79.ru>
2020-08-14 14:38:39 +1000

    Разобрался с API ВТТ

--- vtt_price.php
+++ vtt_price.php
@@ -4,9 +4,9 @@ echo '<br>';
 $wsdl_url = 'http://api.vtt.ru:8048/Portal.svc?singleWsdl'; //ссылка для обращения к API
 $login = 'am-072'; // логин
 $password = '211212'; // пароль
-$f = 10;
-$t = 20;
-$itemId = '832972-001';
+$f = 0;
+$t = 15;
+$itemId = '120012111';
 $data = load_data();
 //write_to_log("Сведения о товарах получены. Всего товаров ".count($data->GetItemsResult->ItemDto));
 echo count($data->GetItemsResult->ItemDto);
@@ -19,21 +19,25 @@ echo '</pre>';
 
 function load_data()
 {
-    global $wsdl_url, $login, $password, $f, $t, $id;
+    global $wsdl_url, $login, $password, $itemId, $f, $t;
     $params = array("login" => $login , "password" => $password);
-    $paramsid = array("login" => $login , "password" => $password, "itemId" => $itemId);
-    //$paramsportion = array("login" => $login , "password" => $password, "f" => $f, "t" => $t);
-    //$paramsportion = array("login" => $login , "password" => $password, "id" => $id);
+    //$paramsid = array("login" => $login , "password" => $password, "itemId" => $itemId);
+    $paramsportion = array("login" => $login , "password" => $password, "from" => $f, "to" => $t);
     try
     {
         $client = new SoapClient($wsdl_url, $params);
-        $dates = $client->GetItem($paramsid);
+        //$dates = $client->GetItem($paramsid);
+        $dates = $client->GetItemPortion($paramsportion);
         return $dates;
     }
     catch (SoapFault $E)
     {
         //write_to_log("Ошибка: ".$E->faultstring);
         echo "Error";
+        echo '<br>';
+        echo $E->faultstring;
+        echo '<br>';
+        print_r($paramsportion);
         die;
     }
 }
